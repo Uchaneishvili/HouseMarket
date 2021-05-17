c271630ad652eb784e8e7237e951b878eb8f1c70
Uchaneishvili <g.uchaneishvili@gmail.com>
2021-05-17 20:04:26 +0400

    swagger is in progress

---
+++ server/Models/Home.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const homeSchema = new mongoose.Schema({
+  image: {
+    // data: Buffer,
+    // contentType: String,
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  room: {
+    type: String,
+    required: true,
+  },
+  floor: {
+    type: String,
+    required: true,
+  },
+  area: {
+    type: Number,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  desc: {
+    type: String,
+  },
+});
+
+const Homes = mongoose.model("Homes", homeSchema);
+module.exports = Homes;
--- server/index.js
+++ server/index.js
@@ -2,6 +2,58 @@ const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const homeModel = require("./Models/Home.js");
+let swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+app.use(express.json());
+app.use(cors());
+
+let swaggerOptions = {
+  swagger: "2.0",
+
+  info: {
+    title: "Home API",
+    description: "Home API information",
+    contact: {
+      name: "Giga Uchaneishvili",
+    },
+    servers: ["http://localhost:3001"],
+  },
+  // ['.routes/*js]
+  apis: ["index.js"],
+};
+
+const swaggerDocs = (swaggerJsDoc = swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+mongoose.connect(
+  "mongodb+srv://guchaneishvili:Gig@20003030@homemarket.toxjt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+  }
+);
+
+//Routes
+
+/**
+ * @swagger
+ * /homelist:
+ *  get:
+ *    description: Use to request all advertisements
+ *    responses:
+ *      '200':
+ *        description: A successfull response
+ *
+ */
+app.get("/homelist", async (req, res) => {
+  await homeModel.find({}, (err, result) => {
+    if (err) {
+      res.send(err);
+    }
+    res.send(result);
+  });
+});
 
 app.listen(3001, () => {
   console.log("running on the port 3001");
